ec50670fe27e4db95b0b910647ac3ec973e3ae87
Luis Mujica <iuris2k@gmail.com>
2021-03-17 16:09:14 -0300

    Desafio 5

--- src/components/ItemListContainer/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ItemListContainer(props) {
-	return <h1>Welcome, {props.greeting}</h1>;
-}
---
+++ src/containers/ItemListContainer/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { ItemCount } from "../../components/ItemCount";
+
+const ItemListContainer = (props) => {
+	const [contador, setContador] = useState(1);
+
+	const onAdd = (stock) => {
+		console.log(stock);
+		if (contador < stock) {
+			setContador(contador + 1);
+		} else {
+			alert("No tenemos mas stock!!!");
+		}
+	};
+
+	const onSubstract = (stock) => {
+		if (contador > 1) {
+			setContador(contador - 1);
+		} else {
+			alert("El valor es menor a lo que podemos vender");
+		}
+	};
+	return (
+		<>
+			<ItemCount
+				stock={5}
+				contador={contador}
+				onSubstract={onSubstract}
+				onAdd={onAdd}
+			/>
+		</>
+	);
+};
+export default ItemListContainer;
