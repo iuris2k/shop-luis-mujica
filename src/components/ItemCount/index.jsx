560d3f1228acb8d5ebacc7589c814440d15cc9a2
Luis Mujica <iuris2k@gmail.com>
2021-03-18 17:34:12 -0300

    Desafio 5c

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -3,7 +3,11 @@ import "./style.css";
 export const ItemCount = ({ stock, onAdd, contador, onSubstract }) => {
 	return (
 		<>
-			<button className="btn btn-primary" onClick={onSubstract}>
+			<button
+				className="btn btn-primary"
+				onClick={onSubstract}
+				disabled={contador === 1}
+			>
 				-
 			</button>
 			<b className="contador">{contador}</b>
@@ -12,6 +16,7 @@ export const ItemCount = ({ stock, onAdd, contador, onSubstract }) => {
 				onClick={() => {
 					onAdd(stock);
 				}}
+				disabled={contador === stock}
 			>
 				+
 			</button>
