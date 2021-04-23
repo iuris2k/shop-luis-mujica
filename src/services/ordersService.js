6cb071c2239104c4e72d8251f7191226197ba6a9
Luis Mujica <iuris2k@gmail.com>
2021-04-23 01:04:47 -0300

    Change to Js

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -6,28 +6,28 @@ const db = getFirestore()
 const ordersCollection = db.collection('orders')
 
 export function getOrders() {
-    return ordersCollection.get().then((snapshot) => {
-        if (snapshot.size > 0) {
-            return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        }
-        return []
-    })
+  return ordersCollection.get().then((snapshot) => {
+    if (snapshot.size > 0) {
+      return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+    }
+    return []
+  })
 }
 
 export function getOrder(orderId) {
-    return ordersCollection
-        .doc(orderId)
-        .get()
-        .then((order) => {
-            if (order.exists) {
-                return order.data()
-            }
-            return {}
-        })
+  return ordersCollection
+    .doc(orderId)
+    .get()
+    .then((order) => {
+      if (order.exists) {
+        return order.data()
+      }
+      return {}
+    })
 }
 
 export function createOrder(orderInfo) {
-    const date = firebase.firestore.Timestamp.fromDate(new Date())
-    const newOrder = {...orderInfo, date }
-    return ordersCollection.add(newOrder).then((orderCreated) => removeFromStocks({...orderInfo, id: orderCreated.id }))
+  const date = firebase.firestore.Timestamp.fromDate(new Date())
+  const newOrder = {...orderInfo, date }
+  return ordersCollection.add(newOrder).then((orderCreated) => removeFromStocks({...orderInfo, id: orderCreated.id }))
 }
